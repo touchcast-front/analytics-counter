faff6688a99d5022dd177334165f82757b52ac44
Seth Silesky <5115498+silesky@users.noreply.github.com>
2023-05-08 16:33:15 -0500

    wip

--- packages/browser/src/lib/pick.ts
+++ packages/browser/src/lib/pick.ts
@@ -7,6 +7,7 @@ export function pick<T extends Record<string, any>, K extends keyof T>(
   object: T,
   keys: readonly K[]
 ): Pick<T, K>
+
 /**
  * @example
  * pick({ 'a': 1, 'b': '2', 'c': 3 }, ['a', 'c'])
