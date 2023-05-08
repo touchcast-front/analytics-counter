af3bc4c751e9759e13f2b7880ad94d7c203645ba
Seth Silesky <5115498+silesky@users.noreply.github.com>
2023-05-08 16:46:01 -0500

    wip

--- packages/browser/src/lib/pick.ts
+++ packages/browser/src/lib/pick.ts
@@ -1,12 +1,12 @@
-export function pick<T extends Record<string, any>>(
-  object: T,
-  keys: string[]
-): Partial<T>
-
 export function pick<T extends Record<string, any>, K extends keyof T>(
   object: T,
   keys: readonly K[]
 ): Pick<T, K>
+
+export function pick<T extends Record<string, any>>(
+  object: T,
+  keys: string[]
+): Partial<T>
 
 /**
  * @example
