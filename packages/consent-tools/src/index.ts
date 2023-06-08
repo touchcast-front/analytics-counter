8c2ed5d77d165166bbda1d168a58503d1c8f1a9a
Seth Silesky <5115498+silesky@users.noreply.github.com>
2023-06-07 23:46:36 -0500

    wip

--- packages/consent-tools/src/index.ts
+++ packages/consent-tools/src/index.ts
@@ -1,1 +1,10 @@
 export { createWrapper } from './domain/create-wrapper'
+export {
+  Wrapper,
+  CreateWrapper,
+  CreateWrapperOptions,
+  RegisterConsentChanged,
+  IntegrationCategoryMappings,
+  Categories,
+  Analytics,
+} from './types'
