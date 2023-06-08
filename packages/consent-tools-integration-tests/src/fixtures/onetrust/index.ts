56b541baa610fed684366b2c9c8feee6d4174c54
Seth Silesky <5115498+silesky@users.noreply.github.com>
2023-06-08 16:01:41 -0500

    wip

--- packages/consent-tools-integration-tests/src/fixtures/onetrust/index.ts
+++ packages/consent-tools-integration-tests/src/fixtures/onetrust/index.ts
@@ -1,1 +1,18 @@
-import {} from '@segment/consent-wrapper-onetrust'
+import { AnalyticsBrowser } from '@segment/analytics-next'
+import { createWrapper } from '@segment/analytics-consent-tools'
+
+const fakeCategories = { Advertising: true, Analytics: true }
+
+const wrap = createWrapper({
+  /* Load + Get initial categories */
+  shouldLoad: () => Promise.resolve(fakeCategories),
+  /* Stamp categories on every event */
+  getCategories: () => fakeCategories,
+})
+
+export const analytics = new AnalyticsBrowser()
+;(window as any).analytics = analytics
+
+wrap(analytics)
+
+analytics.load({ writeKey: '9lSrez3BlfLAJ7NOChrqWtILiATiycoc' })
