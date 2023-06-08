ec7c5ba108ec676d6a73f9021582b921fc8fa268
Seth Silesky <5115498+silesky@users.noreply.github.com>
2023-06-08 15:48:47 -0500

    wip

--- packages/consent-tools-integration-tests/webpack.config.js
+++ packages/consent-tools-integration-tests/webpack.config.js
@@ -1,16 +1,23 @@
 const path = require('path')
+const globby = require('globby')
 
 // This config is for bundling fixtures in order to serve the pages that webdriver.io will use in its tests.
+const files = globby.sync('src/fixtures/*/index.ts', { cwd: __dirname })
+
+const entries = files.reduce((acc, file) => {
+  const [dirName] = file.split('/').slice(-2)
+  const base = path.basename(dirName)
+  return {
+    ...acc,
+    [base]: path.resolve(__dirname, file),
+  }
+}, {})
 
 /** @type { import('webpack').Configuration } */
 const config = {
   mode: process.env.NODE_ENV || 'development',
   devtool: 'source-map',
-  entry: {
-    index: {
-      import: path.resolve(__dirname, 'src/fixtures/basic/index.ts'),
-    },
-  },
+  entry: entries,
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'public/dist'),
