bc76cedd8e4448b8816e4ec5b5b09ab3b73f28fc
Seth Silesky <5115498+silesky@users.noreply.github.com>
2023-06-09 15:58:44 -0500

    wip

--- packages/consent-tools/src/domain/validation/validation-error.ts
+++ packages/consent-tools/src/domain/validation/validation-error.ts
@@ -1,6 +1,4 @@
 export class ValidationError extends Error {
-  type = 'validation'
-
   constructor(message: string, received: any) {
     super(
       `[Validation] ${message} (${`Received: ${JSON.stringify(received)})`}`
